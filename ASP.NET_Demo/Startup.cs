0b4dbf325e6a01c2289026d378a57e7bc638428d
zephyrGates <eric.aj.gates@gmail.com>
2018-04-11 17:52:39 +0800

    Add project files.

---
+++ ASP.NET_Demo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP.NET_Demo.Startup))]
+namespace ASP.NET_Demo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
